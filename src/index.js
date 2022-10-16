297f7af2d537393444f2e12ee87f267d16d55c79
mishatsyhankou <Mikhail_Tsyhankou@epam.com>
2022-10-16 17:36:50 +0400

    start config

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './styles/index.scss';
+
+import React from "react";
+import ReactDom from 'react-dom';
+
+import App from "./Components/App";
+
+ReactDom.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path');
+let mode = 'development'
+
+console.log(process.env.NODE_ENV)
+
+if(process.env.NODE_ENV === 'production'){
+    mode = 'production'
+}
+
+
+module.exports ={
+
+    entry: './src/index.js',
+   mode: mode,
+    output:{
+       filename: '[name].[contenthash].js',
+        path: path.join(__dirname, '/dist'),
+       assetModuleFilename: 'assets/[hash][ext][query]',
+        clean: true,
+    },
+    devServer: {
+        port: 8080,
+        hot: false,
+        liveReload: true,
+    },
+    devtool: 'source-map',
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+
+        new HtmlWebpackPlugin({
+        template: "./src/index.html"
+    })
+    ],
+    module: {
+       rules: [
+           {
+               test: /\.html$/i,
+               loader: 'html-loader',
+           },
+           {
+           test: /\.(sa|sc|c)ss$/,
+           use: [
+               ( mode === 'development')? 'style-loader' : MiniCssExtractPlugin.loader,
+               'css-loader',
+               {
+                   loader: "postcss-loader",
+                   options: {
+                       postcssOptions: {
+                           plugins: [
+                               [
+                                   "postcss-preset-env",
+                                   {
+                                       // Options
+                                   },
+                               ],
+                           ],
+                       },
+                   },
+               },
+               "sass-loader",
+
+
+           ]
+       },
+           {
+               test: /\.(woff|woff2|eot|ttf|otf)$/i,
+               type: 'asset/resource',
+           },
+           {
+               test: /\.(png|svg|jpg|jpeg|gif)$/i,
+               type: 'asset/resource',
+           },
+           {
+               test: /\.(jsx|js)$/,
+               exclude: /node_modules/,
+               use: {
+                   loader: 'babel-loader',
+                   options: {
+                       presets: ['@babel/preset-env', '@babel/preset-react']
+                   }
+               }
+           }
+
+       ],
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx', '.scss', '.tsx']
+    }
+}
