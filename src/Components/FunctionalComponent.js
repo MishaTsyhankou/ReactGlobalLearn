1244ce3195aab3505a0925ab69eaabe238550100
mishatsyhankou <Mikhail_Tsyhankou@epam.com>
2022-10-22 15:14:14 +0400

    Check Husky

--- src/Components/FunctionalComponent.js
+++ src/Components/FunctionalComponent.js
@@ -2,8 +2,23 @@ import React from 'react';
 
 const FunctionalComponent = () => {
     const text = 'Hello React Functional Component!!!!';
+
     console.log('Check Husky');
+
+
+
+
+
     const arr = ['check husky'];
+
+
+
+
     return <h1>{text}</h1>;
+
+
+
+
+
 };
 export default FunctionalComponent;
